1b1d1c51d59ebee0449dd230618fce6877f96eb1
drkarnat <drkarnat@ncsu.edu>
2026-04-19 14:52:20 -0400

    new

--- motion.py
+++ motion.py
@@ -4,15 +4,15 @@ import time
 PIR_PIN = 17
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 print("Ready! Watching for motion (Ctrl+C to stop)\n")
 
 try:
     while True:
         if GPIO.input(PIR_PIN):
-            time.sleep(0.5)  # wait and check again
-            if GPIO.input(PIR_PIN):  # still HIGH? then real motion
+            time.sleep(0.5)
+            if GPIO.input(PIR_PIN):
                 print("Motion detected!")
                 time.sleep(1)
         else:
