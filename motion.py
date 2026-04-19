d9f4b335a8f9739f638f62fb1e840a086eb9db07
drkarnat <drkarnat@ncsu.edu>
2026-04-19 14:25:37 -0400

    starter

---
+++ motion.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+PIR_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+print("Ready! Watching for motion (Ctrl+C to stop)\n")
+
+try:
+    while True:
+        if GPIO.input(PIR_PIN):
+            print("Motion detected!")
+            time.sleep(1)
+        else:
+            print("No motion...")
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nStopped.")
+    GPIO.cleanup()
